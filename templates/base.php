bbfd355a0f5aeb76a3d35ac259ad1f743581333c
Thierry Chappuis <tchappui@osx.chappuis.net>
2017-10-19 23:51:35 +0200

    Fixed indentation in base.php and child.php

--- templates/base.php
+++ templates/base.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title><?php echo $title; ?></title>
-    </head>
-        
-    <body>
-        <?php $block('content'); ?>
-        <p>Welcome in our base template!</p>
-        <?php $endblock(); ?>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+      <title><?php echo $title; ?></title>
+  </head>
+  <body>
+    <?php $block('content'); ?>
+      <p>Welcome in our base template!</p>
+    <?php $endblock(); ?>
+  </body>
 </html>
