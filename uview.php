14c144b553e7539a52af172ed80d75d572134cb2
Thierry Chappuis <tchappui@osx.chappuis.net>
2017-10-19 23:45:31 +0200

    Fixed indentation issues in uview.php

--- uview.php
+++ uview.php
@@ -14,38 +14,38 @@
  */
 function uview(string $_template, array $_data = [], string $_dir = 'templates/')
 {
-	extract($_data);
+    extract($_data);
 
-	$_data['__parent'] =  null;
+    $_data['__parent'] =  null;
 
-	// Starts a block	
-	$block = function($name) use(&$_data) {
-		ob_start(function($buffer) use(&$name, &$_data) {
-			if(empty($_data[$name])) {
-				$_data[$name] = $buffer;
-			}
-			return $_data[$name];
-		});
-	};
+    // Starts a block	
+    $block = function($name) use(&$_data) {
+        ob_start(function($buffer) use(&$name, &$_data) {
+            if(empty($_data[$name])) {
+                $_data[$name] = $buffer;
+            }
+            return $_data[$name];
+        });
+    };
  
-	// Ends a block
-	$endblock = function() use (&$_data) {
-		ob_end_flush();
-	};
+    // Ends a block
+    $endblock = function() use (&$_data) {
+        ob_end_flush();
+    };
 
-	// Extends a template
-	$extends = function($name) use (&$_data) {
-		$_data['__parent'] = $name;
-	};
+    // Extends a template
+    $extends = function($name) use (&$_data) {
+        $_data['__parent'] = $name;
+    };
 		
-	// Rendering of the template
-	ob_start();
-	require("$_dir$_template.php");
-	$_content = trim(ob_get_contents());
-	ob_end_clean();
-	
-	$parent = $_data['__parent'];
+    // Rendering of the template
+    ob_start();
+    require("$_dir$_template.php");
+    $_content = trim(ob_get_contents());
+    ob_end_clean();
 
-	//If a parent exists, uview is called recursively
-	return isset($parent) ? uview($parent, $_data, $_dir) : $_content;
+    $parent = $_data['__parent'];
+
+    //If a parent exists, uview is called recursively
+    return isset($parent) ? uview($parent, $_data, $_dir) : $_content;
 }
