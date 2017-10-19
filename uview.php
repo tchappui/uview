175132a139edc4ccc7860e53f7cbb5c6e4d32cc7
Thierry Chappuis <tchappui@osx.chappuis.net>
2017-10-20 00:05:01 +0200

    Added comments to the content of uvew().

--- uview.php
+++ uview.php
@@ -18,27 +18,28 @@ function uview(string $_template, array $_data = [], string $_dir = 'templates/'
 
     $_data['__parent'] =  null;
 
-    // Starts a block	
+    // Starts a block: captures the content of the block in the _data scope	
+    // Only the content of the top block is stored.
     $block = function($name) use(&$_data) {
-        ob_start(function($buffer) use(&$name, &$_data) {
+        ob_start(function($buf) use(&$name, &$_data) {
             if(empty($_data[$name])) {
-                $_data[$name] = $buffer;
+                $_data[$name] = $buf;
             }
             return $_data[$name];
         });
     };
  
-    // Ends a block
+    // Ends a block: flushes the top-most output buffer and leaves the block
     $endblock = function() use (&$_data) {
         ob_end_flush();
     };
 
-    // Extends a template
+    // Extends a template by setting the name of the parent template
     $extends = function($name) use (&$_data) {
         $_data['__parent'] = $name;
     };
 		
-    // Rendering of the template
+    // Rendering of the template for the cuurent level
     ob_start();
     require("$_dir$_template.php");
     $_content = trim(ob_get_contents());
@@ -46,6 +47,6 @@ function uview(string $_template, array $_data = [], string $_dir = 'templates/'
 
     $parent = $_data['__parent'];
 
-    //If a parent exists, uview is called recursively
+    // If a parent template exists, uview is called recursively
     return isset($parent) ? uview($parent, $_data, $_dir) : $_content;
 }
