e1654990ae27b1380d12df9b9999e169adacbb96
Thierry Chappuis <tchappui@users.noreply.github.com>
2017-10-19 20:59:30 +0200

    Update child.php

--- templates/child.php
+++ templates/child.php
@@ -1,21 +1,21 @@
 <?php $extends('base'); ?>
 
 <?php $block('content'); ?>
-<h1><?php echo $header; ?></h1>
+    <h1><?php echo $header; ?></h1>
 
-<form method="post" action="#">
-    <p>
-        <label for="email">Email: </label>
-        <input id="email" type="email" name="email">
-    </p>
-    <p>
-        <label for="password">Password: </label>
-        <input id="password" type="password" name="password">
-    </p>
-    <p>
-        <input type="submit" value="Submit">
-    </p>
-</form>
+    <form method="post" action="#">
+        <p>
+            <label for="email">Email: </label>
+            <input id="email" type="email" name="email">
+        </p>
+        <p>
+            <label for="password">Password: </label>
+            <input id="password" type="password" name="password">
+        </p>
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+    </form>
 <?php $endblock(); ?>
 
 
